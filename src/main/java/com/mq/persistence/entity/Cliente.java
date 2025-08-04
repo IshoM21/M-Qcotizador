af5beeac551a416f00e72e01f3540f51b3394c72
Misho <varguezisho@gmail.com>
2025-08-04 10:15:48 -0600

    Creacion entidad Cliente

--- src/main/java/com/mq/persistence/entity/Cliente.java
+++ src/main/java/com/mq/persistence/entity/Cliente.java
@@ -2,22 +2,34 @@ package com.mq.persistence.entity;
 
 
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "cliente")
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long  id;
+    private Long id;
 
-    @Column(nullable = false)
     private String razonSocial;
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String rfc;
-
     private String telefono;
     private String email;
     private String complemento;
-    private boolean estadoTimbrado;
+    private int estadoTimbrado;
+    @CreationTimestamp
+    private LocalDateTime fechaRegistro;
+    @UpdateTimestamp
+    private LocalDateTime fechaActualizacion;
 
 }
